08fdc79873cd6740aa53d514c57519a631a6571e
Sean O'Halpin <sean.ohalpin@gmail.com>
2011-07-26 12:08:20 +0100

    added org2textile.rb

---
+++ org2textile.rb
@@ -0,0 +1,179 @@
+#!/usr/bin/env ruby
+# getting to the point where I'll need to build a DOM
+
+$confluence = ARGV.delete("--confluence")
+$APP_DEBUG = ARGV.delete("--debug")
+
+def slide
+  puts "\n!SLIDE\n"
+end
+
+def code_block(line)
+  if $confluence
+    "{code}\n#{line}\n{code}"
+  else
+    "<pre><code>\n#{line}\n</code></pre>"
+  end
+end
+
+def code_span(line)
+  if $confluence
+    "{{#{line}}}"
+  else
+    "@#{line.chomp}@"
+  end
+end
+
+
+def start_code_block
+  if $confluence
+    puts "{code}"
+  else
+    puts "<pre><code>"
+  end
+end
+
+def end_code_block
+  if $confluence
+    puts "{code}"
+  else
+    puts "</code></pre>"
+  end
+end
+
+def expand_link(link)
+  # http://en.wikipedia.com/wiki/
+  scheme, rest = link.split(/:/)
+  case link
+  when /wp:/
+    scheme = "http://en.wikipedia.com/wiki/"
+  when /[a-z+]:/
+    scheme = scheme + ":"
+  end
+  dbg [:scheme, scheme, :rest, rest].inspect
+  scheme + rest.to_s
+end
+
+def dbg(*a)
+  STDERR.puts "DEBUG: " + a.inspect if $APP_DEBUG
+end
+
+in_quote = false
+in_code = false
+spaces = 0
+
+ARGF.each_line do |line|
+  line = line.chomp
+  dbg :input, line
+  case line
+  when /^\s*<.*>\s*$/i
+    # date
+    dbg :case, 1, :angle_date
+    next
+  when /^\s*:\s*(.*)/i
+    # code line
+    dbg :case, 2, :code_line, $1
+    puts code_block($1)
+    next
+  when /^\s*#\+BEGIN_QUOTE/i
+    # start quote
+    dbg :case, 3, :begin_quote
+    in_quote = true
+    next
+  when /^\s*#\+END_QUOTE/i
+    # end quote
+    dbg :case, 4, :end_quote
+    in_quote = false
+    next
+  when /^\s*#\+BEGIN_SRC/i
+    # start code block
+    dbg :case, 5, :begin_src
+    in_code = 0
+    spaces = 0
+    #dbg :BEGIN_SRC
+    start_code_block
+    next
+  when /^\s*#\+END_SRC/i
+    # end code block
+    dbg :case, 6, :end_src
+    in_code = false
+    spaces = 0
+    #dbg :END_SRC
+    end_code_block
+    next
+  when /^\s*#/i
+    # comment
+    dbg :case, 7, :comment
+    #dbg :IGNORE
+    # ignore
+    next
+  when /^\*+ !SLIDE/
+    # SLIDES
+    dbg :case, 8, :slide
+    #dbg :SLIDE
+    slide
+    next
+  when /^\*+/
+    # headings
+    dbg :case, 9, :heading
+    line = line.gsub(/^(\*+)(\s+.*$)/) do |m|
+      "\n\nh#{$1.size}.#{$2}\n\n"
+    end
+  when /^\|\-+/
+    dbg :case, 10, :table_header
+    # skip table header lines
+    next
+  when /^\s*\-+\s+/
+    # bullet list
+    dbg :case, 11, :bullet_list_item
+    line = line.gsub(/^(\s*\-)/) do |m|
+      "*" * (($1.size + 1) / 2)
+    end
+  end
+
+  # replace links
+
+  line = line.gsub(/\[\[(.*?)\]\]/){|match|
+    link, desc = $1.split(/\]\[/)
+    dbg [:match, match, :one, $1, :link, link, :desc, desc].inspect
+    link = expand_link(link)
+    if desc
+      "[#{desc}|#{link}]"
+    else
+      "[##{link}]"
+    end
+  }
+
+  # formatting
+  line = line.gsub(/\s\/(.+?)\/\s/){|match|
+    # make sure not inside link
+    " _#{$1}_ "
+  }
+
+  # bit crude but works (mostly)
+  line = line.gsub(/\s=(.*?)=/, ' {{\\1}}')
+
+  if in_code
+    dbg :case, :in_code
+    if in_code == 0
+      m = line.match(/^\s+/)
+      spaces = m.to_s.size
+    end
+    # p [:line, line, :spaces, spaces]
+    #if $confluence
+      puts line[spaces..-1]
+    #else
+    #  puts (" " * 4) + line
+    #end
+    #puts "#{in_code}: #{line}"
+    in_code += 1
+  else
+    if in_quote
+      dbg :case, :in_quote
+      print "bq. "
+    end
+    dbg :output, line
+    puts line
+  end
+
+end
